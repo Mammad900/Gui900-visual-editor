e1dcceefaeff3c2d04999ee0ee5b3a542f10794e
Mammad900 <mohammadhosseinabedi052@gmail.com>
2020-12-10 18:05:08 +0330

    Draw elements in preview box
    Only the consumer part is implemented,
    Provider parts are not yet created

--- res/preview/preview.js
+++ res/preview/preview.js
@@ -4,5 +4,8 @@ var preview={
         var ctx= $("#preview-canvas")[0].getContext("2d");
         ctx.fillStyle = $("#page_bc_color_input").val();
         ctx.fillRect(0, 0, 240, 320);
+        elements.data.forEach(function (val, index) {
+            elements.types[val.type].generatePreview(index,$("#preview-canvas"));
+        })
     },
 }
