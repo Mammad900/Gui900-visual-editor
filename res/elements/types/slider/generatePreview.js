f015ad8321222c00ca500439d3806616d47fa3c0
Mammad900 <mohammadhosseinabedi052@gmail.com>
2020-12-12 22:51:06 +0330

    Do not render slider preview if it is invisible

--- res/elements/types/slider/generatePreview.js
+++ res/elements/types/slider/generatePreview.js
@@ -6,9 +6,10 @@ elements.types["Slider"].generatePreview=
  * @param {JQuery<HTMLCanvasElement>} canvas 
  */
 function (index,canvas) {
+    var slider=elements.data[index];
+    if(!slider.visible)return;
     var ctx=canvas[0].getContext("2d");
     ctx.imageSmoothingEnabled=false;
-    var slider=elements.data[index];
     var x=slider.position.x, y=slider.position.y, w=slider.size.width, h=slider.size.height;
     var tw= slider.thumbWidth;
     var val_px=preview.map(slider.value, slider.min, slider.max, 0, w-2-tw);
