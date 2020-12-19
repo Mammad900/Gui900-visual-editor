e5b96c9660db09282cb7d40eacf993ae38f832cb
Mammad <67197540+Mammad900@users.noreply.github.com>
2020-12-19 11:37:10 +0330

    Move elements table buttons event handlers to the 'actions' object
    It makes it possible to call them from outside

--- res/elements/ui/table/buttons.js
+++ res/elements/ui/table/buttons.js
@@ -11,98 +11,15 @@ elements.table={
 
             var b1=elements.table.buttons.generateButton(
                 `<i class="fas fa-arrow-up"></i>`,
-                "Move up", function(){
-                    var e=$(this).parent().parent().parent().parent();
-                    e.prev().insertAfter(e);
-                    var i=$("#elements_table tr").index(e)-1;
-
-                    var arr=elements.table.rows.data[i];
-                    elements.table.rows.data[i]=elements.table.rows.data[i+1];
-                    elements.table.rows.data[i+1]=arr;
-                    if(i==0){
-                        elements.table.rows.data[0][0].prop("disabled",true);
-                        elements.table.rows.data[1][0].prop("disabled",false);
-                    }
-                    var n=elements.table.rows.data.length-1;
-                    if(i+1==n){
-                        elements.table.rows.data[n][1].prop("disabled",true);
-                        elements.table.rows.data[n-1][1].prop("disabled",false);
-                    }
-                    elements.table.rows.sortNumbers();
-                    
-                    var arr2=elements.data[i];
-                    elements.data[i]=elements.data[i+1];
-                    elements.data[i+1]=arr2;
-                });
+                "Move up", elements.table.buttons.actions.moveUp);
             container.append(b1[0]);
             var b2=elements.table.buttons.generateButton(
                 `<i class="fas fa-arrow-down"></i>`,
-                "Move down", function(){
-                    var e=$(this).parent().parent().parent().parent();
-                    e.next().insertBefore(e);
-                    var i=$("#elements_table tr").index(e)-1;
-
-                    var arr=elements.table.rows.data[i];
-                    elements.table.rows.data[i]=elements.table.rows.data[i-1];
-                    elements.table.rows.data[i-1]=arr;
-
-                    if(i==1){
-                        elements.table.rows.data[0][0].prop("disabled",true);
-                        elements.table.rows.data[1][0].prop("disabled",false);
-                    }
-                    var n=elements.table.rows.data.length-1;
-                    if(i++==n){
-                        elements.table.rows.data[n][1].prop("disabled",true);
-                        elements.table.rows.data[n-1][1].prop("disabled",false);
-                    }
-                    elements.table.rows.sortNumbers();
-
-                    var arr2=elements.data[i];
-                    elements.data[i]=elements.data[i-1];
-                    elements.data[i-1]=arr2;
-                });
+                "Move down", elements.table.buttons.actions.moveDown);
             container.append(b2[0]);
             var b3=elements.table.buttons.generateButton(
                 `<i class="fas fa-times"></i>`,
-                "Delete element", function(){
-                    var row=$(this);
-                    dialog.new("Do you really want to <strong>delete</strong> this element?<br>This cannot be undone.",[
-                        {
-                            "id": 0,
-                            "text": "No",
-                            "key": 27
-                        },
-                        {
-                            "id": 1,
-                            "text": "Yes",
-                            "key": 13
-                        }
-                    ],function (id,e) {
-                        if(id==1){
-                            if(elements.selectedElement!=-1)
-                                elements.types[elements.data[elements.selectedElement].type].saveProperties(elements.selectedElement);
-                            properties.getElement().children().remove();
-                            $("#properties-header-details").text("No item selected");
-
-                            var tr= row.parent().parent().parent().parent();
-                            var i=$("#elements_table tr").index(tr)-1;
-                            tr.remove();
-                            elements.table.rows.data.splice(i, 1)
-                            if(!elements.table.rows.data.length==0){
-                                if(i==0){
-                                    elements.table.rows.data[0][0].prop("disabled",true)
-                                }
-                                if(i==elements.table.rows.data.length){
-                                    elements.table.rows.data[i-1][1].prop("disabled",true);
-                                }
-                            }
-                            elements.data.splice(i, 1)
-                            elements.selectedElement=-1;
-                        }
-                        elements.table.rows.sortNumbers();
-                        dialog.close();
-                    })
-                })
+                "Delete element", elements.table.buttons.actions.delete)
             container.append(b3[0]);
 
             td.append(container);
@@ -129,6 +46,94 @@ elements.table={
             span.append(button);
 
             return [span,button];
+        },
+        actions: {
+            moveUp: function(){
+                var e=$(this).parent().parent().parent().parent();
+                e.prev().insertAfter(e);
+                var i=$("#elements_table tr").index(e)-1;
+
+                var arr=elements.table.rows.data[i];
+                elements.table.rows.data[i]=elements.table.rows.data[i+1];
+                elements.table.rows.data[i+1]=arr;
+                if(i==0){
+                    elements.table.rows.data[0][0].prop("disabled",true);
+                    elements.table.rows.data[1][0].prop("disabled",false);
+                }
+                var n=elements.table.rows.data.length-1;
+                if(i+1==n){
+                    elements.table.rows.data[n][1].prop("disabled",true);
+                    elements.table.rows.data[n-1][1].prop("disabled",false);
+                }
+                elements.table.rows.sortNumbers();
+                
+                var arr2=elements.data[i];
+                elements.data[i]=elements.data[i+1];
+                elements.data[i+1]=arr2;
+            },
+            moveDown: function(){
+                var e=$(this).parent().parent().parent().parent();
+                e.next().insertBefore(e);
+                var i=$("#elements_table tr").index(e)-1;
+
+                var arr=elements.table.rows.data[i];
+                elements.table.rows.data[i]=elements.table.rows.data[i-1];
+                elements.table.rows.data[i-1]=arr;
+
+                if(i==1){
+                    elements.table.rows.data[0][0].prop("disabled",true);
+                    elements.table.rows.data[1][0].prop("disabled",false);
+                }
+                var n=elements.table.rows.data.length-1;
+                if(i++==n){
+                    elements.table.rows.data[n][1].prop("disabled",true);
+                    elements.table.rows.data[n-1][1].prop("disabled",false);
+                }
+                elements.table.rows.sortNumbers();
+
+                var arr2=elements.data[i];
+                elements.data[i]=elements.data[i-1];
+                elements.data[i-1]=arr2;
+            },
+            delete: function(){
+                var row=$(this);
+                dialog.new("Do you really want to <strong>delete</strong> this element?<br>This cannot be undone.",[
+                    {
+                        "id": 0,
+                        "text": "No",
+                        "key": 27
+                    },
+                    {
+                        "id": 1,
+                        "text": "Yes",
+                        "key": 13
+                    }
+                ],function (id,e) {
+                    if(id==1){
+                        if(elements.selectedElement!=-1)
+                            elements.types[elements.data[elements.selectedElement].type].saveProperties(elements.selectedElement);
+                        properties.getElement().children().remove();
+                        $("#properties-header-details").text("No item selected");
+
+                        var tr= row.parent().parent().parent().parent();
+                        var i=$("#elements_table tr").index(tr)-1;
+                        tr.remove();
+                        elements.table.rows.data.splice(i, 1)
+                        if(!elements.table.rows.data.length==0){
+                            if(i==0){
+                                elements.table.rows.data[0][0].prop("disabled",true)
+                            }
+                            if(i==elements.table.rows.data.length){
+                                elements.table.rows.data[i-1][1].prop("disabled",true);
+                            }
+                        }
+                        elements.data.splice(i, 1)
+                        elements.selectedElement=-1;
+                    }
+                    elements.table.rows.sortNumbers();
+                    dialog.close();
+                })
+            }
         }
     }
 };
