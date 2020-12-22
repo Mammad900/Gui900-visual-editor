5fa642cfe9ff19c6aca7aae138e987dc272de582
Mammad <67197540+Mammad900@users.noreply.github.com>
2020-12-22 14:36:54 +0330

    Fix numeric values for labels
    Numeric values were stored as strings

--- res/elements/types/label/saveProperties.js
+++ res/elements/types/label/saveProperties.js
@@ -4,21 +4,24 @@ elements.types["Label"].saveProperties=function (index) {
     function g(id) {
         return $("#"+id).val();
     }
+    function n(id) {
+        return $("#"+id)[0].valueAsNumber;
+    }
     function b(id) {
         return (($("#"+id).data("checked"))=="1");
     }
     elements.data[index]={
         type: "Label",
         position: {
-            x: g("property-position-x"),
-            y: g("property-position-y")
+            x: n("property-position-x"),
+            y: n("property-position-y")
         },
         text: g("property-text"),
         color: g("property-color"),
         font: g("property-font"),
         text_size: {
-            x: g("property-size-x"),
-            y: g("property-size-y"),
+            x: n("property-size-x"),
+            y: n("property-size-y"),
         },
         enabled: b("property-enabled"),
         visible: b("property-visible")
