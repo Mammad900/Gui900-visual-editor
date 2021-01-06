f006df31ef2511a9f09832ae3f2e7bf2fb5fc827
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-01-06 09:43:45 +0330

    Display a warning if brightness is too low and is simulated in preview

--- res/settings/fields/screen-brightness.js
+++ res/settings/fields/screen-brightness.js
@@ -4,7 +4,10 @@ function LP_GV_SettingsFields_ScreenBrightness(){
         save: function (value, preview) {
             settings.data.screenBrightness.value=value;
             settings.data.screenBrightness.simulateInPreview=preview;
-            $("#preview-canvas").css('filter',(preview?('brightness('+(value/255)+')'):'none'))
+            if((value<20)&(preview)){
+                notification.warning("Preview can be unreadable with so low brightness, it is recommended to either disable emulating brightness, or increase brightness.")
+            }
+            $("#preview-canvas").css('filter',(preview?('brightness('+(value/255)+')'):'none'));
         }
     }
 }
