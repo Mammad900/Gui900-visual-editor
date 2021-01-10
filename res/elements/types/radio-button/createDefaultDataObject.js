d03e5b433654d025b0852d2b1b1a08823edac8b2
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-01-10 20:28:19 +0330

    Change radio-button default colors

--- res/elements/types/radio-button/createDefaultDataObject.js
+++ res/elements/types/radio-button/createDefaultDataObject.js
@@ -12,9 +12,9 @@ function LP_GV_El_RadioButton_1(){
                 colors: {
                     text: "#ffffff",
                     box: {
-                        background: "#000000",
-                        border: "#ffffff",
-                        indicator: "#ffffff"
+                        background: "#ffffff",
+                        border: "#000000",
+                        indicator: "#222222"
                     }
                 },
                 size: 2,
