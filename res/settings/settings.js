1200c38be87f9db356380ec7ffd1520da87bdde3
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-01-11 16:46:38 +0330

    Implement touch calibration setting fields
    TS_LEFT, TS_TOP, TS_RT, TS_BOT,
    XP, XM, YP, YM,
    MINPRESSURE, MAXPRESSURE

--- res/settings/settings.js
+++ res/settings/settings.js
@@ -44,6 +44,24 @@ var settings= {
                 3:"Landscape reverse"
             }, String(settings.data.orientation));
         });
+        settings.gen.fieldset(div, "Touch settings",function (fs) {
+            settings.gen.grid(fs, 4, 1, [
+                function (iC) { settings.gen.inputNumber(iC, "TS_LEFT", "settings-touchCalibration-TS_LEFT", 0,  1023, settings.data.touchCalibration.TS_LEFT);},
+                function (iC) { settings.gen.inputNumber(iC, "TS_TOP" , "settings-touchCalibration-TS_TOP" , 0,  1023, settings.data.touchCalibration.TS_TOP );},
+                function (iC) { settings.gen.inputNumber(iC, "TS_RT"  , "settings-touchCalibration-TS_RT"  , 0,  1023, settings.data.touchCalibration.TS_RT  );},
+                function (iC) { settings.gen.inputNumber(iC, "TS_BOT" , "settings-touchCalibration-TS_BOT" , 0,  1023, settings.data.touchCalibration.TS_BOT ); },
+            ],true);
+            settings.gen.grid(fs, 4, 1, [
+                function (iC) { settings.gen.inputText(iC, "XP", "settings-touchCalibration-XP", settings.data.touchCalibration.XP); },
+                function (iC) { settings.gen.inputText(iC, "XM", "settings-touchCalibration-XM", settings.data.touchCalibration.XM); },
+                function (iC) { settings.gen.inputText(iC, "YP", "settings-touchCalibration-YP", settings.data.touchCalibration.YP); },
+                function (iC) { settings.gen.inputText(iC, "YM", "settings-touchCalibration-YM", settings.data.touchCalibration.YM); },
+            ],true);
+            settings.gen.grid(fs, 2, 1, [
+                function (iC) { settings.gen.inputNumber(iC, "MINPRESSURE", "settings-touchCalibration-MINPRESSURE", 0,  10000, settings.data.touchCalibration.MINPRESSURE); },
+                function (iC) { settings.gen.inputNumber(iC, "MAXPRESSURE", "settings-touchCalibration-MAXPRESSURE", 0,  10000, settings.data.touchCalibration.MAXPRESSURE); },
+            ],true);
+        })
     },
     saveSettings: function () {
         function g(id) {
@@ -58,6 +76,13 @@ var settings= {
         settings.data.orientation= Number(g("settings-orientation"));
         settings.fields.screenBrightness.save(n('settings-screen-brightness'),b('settings-screen-brightness-preview'))
         settings.fields.screenSize.save(n("settings-screen-width"),n("settings-screen-height"))
+        settings.data.touchCalibration= {
+            XP: g("settings-touchCalibration-XP"), XM: g("settings-touchCalibration-XM"),
+            YP: g("settings-touchCalibration-YP"), YM: g("settings-touchCalibration-YM"),
+            MINPRESSURE: n("settings-touchCalibration-MINPRESSURE"), MAXPRESSURE: n("settings-touchCalibration-MAXPRESSURE"),
+            TS_LEFT: n("settings-touchCalibration-TS_LEFT"), TS_RT: n("settings-touchCalibration-TS_RT"),
+            TS_TOP: n("settings-touchCalibration-TS_TOP"), TS_BOT: n("settings-touchCalibration-TS_BOT")
+        }
     },
     fields:{},
     data:{
@@ -69,7 +94,12 @@ var settings= {
             value: 255,
             simulateInPreview: true
         },
-        orientation: 0
+        orientation: 0,
+        touchCalibration:{
+            XP: "6",   XM: "A2",   YP: "A1",   YM: "7",
+            MINPRESSURE: 10,  MAXPRESSURE: 1000,
+            TS_LEFT: 1023, TS_RT: 0, TS_TOP: 0, TS_BOT: 1023
+        }
     }
 }
 function LP_GV_SettingsGenerator() {
