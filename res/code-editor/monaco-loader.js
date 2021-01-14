e5ec06429ab0e0303b702cb7d400183a3228a4ab
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-01-14 15:12:26 +0330

    All themes now have a code editor color theme

--- res/code-editor/monaco-loader.js
+++ res/code-editor/monaco-loader.js
@@ -7,16 +7,18 @@ loadJsCssFile("res/code-editor/monaco-editor/min/vs/editor/editor.main.js");
 var monacoInstance=undefined;
 
 function LP_LoadEditor() {
-    monaco.editor.defineTheme('GVE-dark', {
-        base: 'vs-dark',
-        inherit: true,
-        rules: [{  }],
-    });
-    monaco.editor.defineTheme('GVE-light', {
-        base: 'vs',
-        inherit: true,
-        rules: [{  }],
-    });
+    monaco.editor.defineTheme('GVE-dark', { base: 'vs-dark', inherit: true, rules: [{  }], });
+    monaco.editor.defineTheme('GVE-dark-red'   , { base: 'vs-dark', inherit: true, rules: [{ background: "200000" }], colors: { "editor.background": "#200000" } });
+    monaco.editor.defineTheme('GVE-dark-green' , { base: 'vs-dark', inherit: true, rules: [{ background: "002000" }], colors: { "editor.background": "#002000" } });
+    monaco.editor.defineTheme('GVE-dark-blue'  , { base: 'vs-dark', inherit: true, rules: [{ background: "000020" }], colors: { "editor.background": "#000020" } });
+    monaco.editor.defineTheme('GVE-dark-yellow', { base: 'vs-dark', inherit: true, rules: [{ background: "202000" }], colors: { "editor.background": "#202000" } });
+    monaco.editor.defineTheme('GVE-light-red'   , { base: 'vs', inherit: true, rules: [{ background: "ff8e8e" }], colors: { "editor.background": "#ff8e8e" } });
+    monaco.editor.defineTheme('GVE-light-green' , { base: 'vs', inherit: true, rules: [{ background: "8eff8e" }], colors: { "editor.background": "#8eff8e" } });
+    monaco.editor.defineTheme('GVE-light-blue'  , { base: 'vs', inherit: true, rules: [{ background: "8e8eff" }], colors: { "editor.background": "#8e8eff" } });
+    monaco.editor.defineTheme('GVE-light-yellow', { base: 'vs', inherit: true, rules: [{ background: "ffff8e" }], colors: { "editor.background": "#ffff8e" } });
+    monaco.editor.defineTheme('GVE-high-contrast', { base: 'hc-black', inherit: true, rules: [{  }], });
+    monaco.editor.defineTheme('GVE-windows-10-UWP-dark-style', { base: 'vs-dark', inherit: true, rules: [{ background: "000000" }], colors: { "editor.background": "#000000" } });
+    monaco.editor.defineTheme('GVE-light', { base: 'vs', inherit: true, rules: [{  }], });
 
     monacoInstance= monaco.editor.create($("#monaco-container")[0],{
         language: "cpp",
