8c65d9788d3c906b1bb8829d46acdc8c77dca72c
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-01-26 13:24:28 +0330

    [internal] Pages table rows buttons handlers are now named functions.
    Did this so they're accesible from outside.
    They're located in pages.table.buttons.actions

--- res/pages/ui/table/buttons.js
+++ res/pages/ui/table/buttons.js
@@ -6,121 +6,11 @@ pages.table= {
             var container=$("<div></div>");
             container.addClass("buttons");
 
-            var b1=pages.table.buttons.generateButton(
-                `<i class="fas fa-arrow-up"></i>`,
-                "Move up", function(){
-                    var e=$(this).parent().parent().parent().parent();
-                    e.prev().insertAfter(e);
-                    var i=$("#pages_table tr").index(e)-1;
-
-                    var arr=pages.table.rows.data[i];
-                    pages.table.rows.data[i]=pages.table.rows.data[i+1];
-                    pages.table.rows.data[i+1]=arr;
-
-                    var arr2=pages.data[i];
-                    pages.data[i]=pages.data[i+1];
-                    pages.data[i+1]=arr2;
-                    
-                    if(pages.currentPage==i){
-                        pages.currentPage++;
-                    }
-                    else if(pages.currentPage==i+1){
-                        pages.currentPage--;
-                    }
-
-                    if(i==0){
-                        pages.table.rows.data[0][0].prop("disabled",true);
-                        pages.table.rows.data[1][0].prop("disabled",false);
-                    }
-                    var n=pages.table.rows.data.length-1;
-                    if(i+1==n){
-                        pages.table.rows.data[n][1].prop("disabled",true);
-                        pages.table.rows.data[n-1][1].prop("disabled",false);
-                    }
-                    pages.table.rows.sortNumbers();
-                });
+            var b1=pages.table.buttons.generateButton(`<i class="fas fa-arrow-up"></i>`,"Move up", pages.table.buttons.actions.moveUp);
             container.append(b1[0]);
-            var b2=pages.table.buttons.generateButton(
-                `<i class="fas fa-arrow-down"></i>`,
-                "Move down", function(){
-                    var e=$(this).parent().parent().parent().parent();
-                    e.next().insertBefore(e);
-                    var i=$("#pages_table tr").index(e)-1;
-
-                    var arr=pages.table.rows.data[i];
-                    pages.table.rows.data[i]=pages.table.rows.data[i-1];
-                    pages.table.rows.data[i-1]=arr;
-
-                    var arr2=pages.data[i];
-                    pages.data[i]=pages.data[i-1];
-                    pages.data[i-1]=arr2;
-                    
-                    if(pages.currentPage==i){
-                        pages.currentPage--;
-                    }
-                    else if(pages.currentPage==i-1){
-                        pages.currentPage++;
-                    }
-
-                    if(i==1){
-                        pages.table.rows.data[0][0].prop("disabled",true);
-                        pages.table.rows.data[1][0].prop("disabled",false);
-                    }
-                    var n=pages.table.rows.data.length-1;
-                    if(i++==n){
-                        pages.table.rows.data[n][1].prop("disabled",true);
-                        pages.table.rows.data[n-1][1].prop("disabled",false);
-                    }
-                    pages.table.rows.sortNumbers();
-                });
+            var b2=pages.table.buttons.generateButton(`<i class="fas fa-arrow-down"></i>`, "Move down", pages.table.buttons.actions.moveDown);
             container.append(b2[0]);
-            var b3=pages.table.buttons.generateButton(
-                `<i class="fas fa-times"></i>`,
-                "Delete page", function(){
-                    var row=$(this);
-                    dialog("Do you really want to <strong>delete</strong> this page?<br>This cannot be undone.",[
-                        {
-                            "id": 0,
-                            "text": "No",
-                            "key": 27
-                        },
-                        {
-                            "id": 1,
-                            "text": "Yes",
-                            "key": 13
-                        }
-                    ],function (id,e) {
-                        if(id==1){
-                            var tr= row.parent().parent().parent().parent();
-                            var i=$("#pages_table tr").index(tr)-1;
-                            var ib=false;
-                            if(i==pages.currentPage){
-                                pages.selectPage((i==0)?1:0);
-                            }
-                            else{
-                                ib=i<pages.currentPage;
-                            }
-
-                            tr.remove();
-                            pages.table.rows.data.splice(i, 1)
-                            if(!pages.table.rows.data.length==0){
-                                if(i==0){
-                                    pages.table.rows.data[0][0].prop("disabled",true)
-                                }
-                                if(i==pages.table.rows.data.length){
-                                    pages.table.rows.data[i-1][1].prop("disabled",true);
-                                }
-                            }
-
-                            pages.data.splice(i, 1);
-                            if(ib){
-                                pages.currentPage--;
-                            }
-                        }
-                        pages.table.rows.sortNumbers();
-                        dialog.close();
-                    })
-                })
+            var b3=pages.table.buttons.generateButton(`<i class="fas fa-times"></i>`, "Delete page",pages.table.buttons.actions.delete);
             container.append(b3[0]);
 
             td.append(container);
@@ -140,6 +30,115 @@ pages.table= {
             span.append(button);
 
             return [span,button];
+        },
+        actions: {
+            delete: function(){
+                var row=$(this);
+                dialog("Do you really want to <strong>delete</strong> this page?<br>This cannot be undone.",[
+                    {
+                        "id": 0,
+                        "text": "No",
+                        "key": 27
+                    },
+                    {
+                        "id": 1,
+                        "text": "Yes",
+                        "key": 13
+                    }
+                ],function (id,e) {
+                    if(id==1){
+                        var tr= row.parent().parent().parent().parent();
+                        var i=$("#pages_table tr").index(tr)-1;
+                        var ib=false;
+                        if(i==pages.currentPage){
+                            pages.selectPage((i==0)?1:0);
+                        }
+                        else{
+                            ib=i<pages.currentPage;
+                        }
+
+                        tr.remove();
+                        pages.table.rows.data.splice(i, 1)
+                        if(!pages.table.rows.data.length==0){
+                            if(i==0){
+                                pages.table.rows.data[0][0].prop("disabled",true)
+                            }
+                            if(i==pages.table.rows.data.length){
+                                pages.table.rows.data[i-1][1].prop("disabled",true);
+                            }
+                        }
+
+                        pages.data.splice(i, 1);
+                        if(ib){
+                            pages.currentPage--;
+                        }
+                    }
+                    pages.table.rows.sortNumbers();
+                    dialog.close();
+                })
+            },
+            moveDown: function(){
+                var e=$(this).parent().parent().parent().parent();
+                e.next().insertBefore(e);
+                var i=$("#pages_table tr").index(e)-1;
+
+                var arr=pages.table.rows.data[i];
+                pages.table.rows.data[i]=pages.table.rows.data[i-1];
+                pages.table.rows.data[i-1]=arr;
+
+                var arr2=pages.data[i];
+                pages.data[i]=pages.data[i-1];
+                pages.data[i-1]=arr2;
+                
+                if(pages.currentPage==i){
+                    pages.currentPage--;
+                }
+                else if(pages.currentPage==i-1){
+                    pages.currentPage++;
+                }
+
+                if(i==1){
+                    pages.table.rows.data[0][0].prop("disabled",true);
+                    pages.table.rows.data[1][0].prop("disabled",false);
+                }
+                var n=pages.table.rows.data.length-1;
+                if(i++==n){
+                    pages.table.rows.data[n][1].prop("disabled",true);
+                    pages.table.rows.data[n-1][1].prop("disabled",false);
+                }
+                pages.table.rows.sortNumbers();
+            },
+            moveUp: function(){
+                var e=$(this).parent().parent().parent().parent();
+                e.prev().insertAfter(e);
+                var i=$("#pages_table tr").index(e)-1;
+
+                var arr=pages.table.rows.data[i];
+                pages.table.rows.data[i]=pages.table.rows.data[i+1];
+                pages.table.rows.data[i+1]=arr;
+
+                var arr2=pages.data[i];
+                pages.data[i]=pages.data[i+1];
+                pages.data[i+1]=arr2;
+                
+                if(pages.currentPage==i){
+                    pages.currentPage++;
+                }
+                else if(pages.currentPage==i+1){
+                    pages.currentPage--;
+                }
+
+                if(i==0){
+                    pages.table.rows.data[0][0].prop("disabled",true);
+                    pages.table.rows.data[1][0].prop("disabled",false);
+                }
+                var n=pages.table.rows.data.length-1;
+                if(i+1==n){
+                    pages.table.rows.data[n][1].prop("disabled",true);
+                    pages.table.rows.data[n-1][1].prop("disabled",false);
+                }
+                pages.table.rows.sortNumbers();
+            }
         }
     }
 };
