26a6d25f95806ab9f34c654c9a2ac3e6700764bf
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-02-14 17:04:33 +0330

    Version 0.1.0

--- res/ui/toolbar/handlers/help/about.js
+++ res/ui/toolbar/handlers/help/about.js
@@ -3,7 +3,7 @@ function LP_GV_Toolbar_Handler_3(){
     toolBar.handlers.help= {
         about: function (e) {
             dialog(
-                `Gui900 UI Editor 0.0.6<br>
+                `Gui900 UI Editor 0.1.0<br>
                 By <a href="https://github.com/Mammad900" target="_blank">Mammad900</a><br>
                 JQuery 3.5.1<br>
                 Monaco editor 0.21.3<br>
