a753f906225b89016bb3310c355ab2e6dcf8fa77
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-02-15 16:05:59 +0330

    Include JQuery UI version in about dialog

--- res/ui/toolbar/handlers/help/about.js
+++ res/ui/toolbar/handlers/help/about.js
@@ -6,6 +6,7 @@ function LP_GV_Toolbar_Handler_3(){
                 `Gui900 UI Editor 0.1.0<br>
                 By <a href="https://github.com/Mammad900" target="_blank">Mammad900</a><br>
                 JQuery 3.5.1<br>
+                JQuery UI 1.10.3<br>
                 Monaco editor 0.21.3<br>
                 Font Awesome Free 5.15.1`,[
                 {
