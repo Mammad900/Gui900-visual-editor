29050251ed3f8c9aa88840c5210b7f9019d4ccd1
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-02-25 16:45:52 +0330

    Remove unused code

--- res/ui/box/manageBoxes.js
+++ res/ui/box/manageBoxes.js
@@ -1,26 +1,3 @@
-/**
- * 
- * @param {JQuery<HTMLElement>} one 
- * @param {JQuery<HTMLElement>} two 
- */
-function swapBox(one, two, areDirectSiblings=false) {
-    var par=one.parent();
-    var i=par.children().index(one);
-    one.detach();
-    if(i!=0){
-        two.after(one);
-    }else{
-        two.before(one);
-    }
-    two.detach();
-    if(i==0){
-        par.prepend(two);
-    }else{
-        par.append(two);
-    }
-}
-
-
 var floatingBoxes={};
 /**
  * 
