Mammad900/Gui900-visual-editor

commit 8cb12bfe5fa004bdb77ec561ade674883e54067a

[internal] Refactor toolbar code for better structure

res/ui/toolbar/handlers/file/open-from-file.js
@@ -1,9 +1,13 @@
 'use strict';
-function LP_GV_Toolbar_Handler_2(){
-    toolBar.handlers.open_from_file=function (e) {
-        $("#fileinput").trigger("click");
-    }
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.file= toolBar.handlers.file || {};
+toolBar.handlers.file.open= toolBar.handlers.file.open || {};
+
+toolBar.handlers.file.open_from_file=function (e) {
+    $("#fileinput").trigger("click");
 }
+
 function LP_CreateHiddenFileInput(){
     $(document.body).append($('<input type="file" class="hidden" name="fileinput" id="fileinput"/>'));
     $('#fileinput').on('change', function() { 

res/ui/toolbar/handlers/code/generateLoopCode.js
@@ -1,67 +1,67 @@
 'use strict';
-function LP_GV_Toolbar_Handler_8(){
-    toolBar.handlers.code= {
-        generateLoopCode (e) {
-            var lc=GenerateLoopCode();
-            var code= projectCode.loop;
-            var goneToDialog=false;
-            if(code.trim()==""){
-                code=lc;
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.code= toolBar.handlers.code || {};
+
+toolBar.handlers.code.generateLoopCode = function(e) {
+    var lc=GenerateLoopCode();
+    var code= projectCode.loop;
+    var goneToDialog=false;
+    if(code.trim()==""){
+        code=lc;
+    }
+    else if(code.includes("// GENERATED LOOP CODE") && code.includes("// END GENERATED LOOP CODE")){
+        var exC=code.slice(code.indexOf("// GENERATED LOOP CODE"),code.indexOf("// END GENERATED LOOP CODE")+26);
+        code=code.replace(exC,lc);
+    }
+    else{
+        goneToDialog=true;
+        dialog("Where do you want the generated loop code to be placed?",[
+            {
+                "id": 0,
+                "text": "End of loop()"
+            },
+            {
+                "id": 1,
+                "text": "Beginning of loop()"
+            },
+            {
+                "id": 2,
+                "text": "Replace all code in loop()"
+            },
+            {
+                "id": 3,
+                "text": "Cancel"
             }
-            else if(code.includes("// GENERATED LOOP CODE") && code.includes("// END GENERATED LOOP CODE")){
-                var exC=code.slice(code.indexOf("// GENERATED LOOP CODE"),code.indexOf("// END GENERATED LOOP CODE")+26);
-                code=code.replace(exC,lc);
-            }
-            else{
-                goneToDialog=true;
-                dialog("Where do you want the generated loop code to be placed?",[
-                    {
-                        "id": 0,
-                        "text": "End of loop()"
-                    },
-                    {
-                        "id": 1,
-                        "text": "Beginning of loop()"
-                    },
-                    {
-                        "id": 2,
-                        "text": "Replace all code in loop()"
-                    },
-                    {
-                        "id": 3,
-                        "text": "Cancel"
-                    }
-                ],function (id,e) {
-                    switch (id) {
-                        case 0:
-                            code+=lc;
-                            break;
-                        case 1:
-                            code=lc+"\n"+code;
-                            break;
-                        case 2:
-                            code=lc;
-                            break;
-                        case 3:
-                        default:
-                            dialog.close();
-                            return;
-                    }
-                    projectCode.loop=code;
-                    if($("#codeParts span[data-selected=true]").attr("data-name")=="loop"){
-                        monacoInstance.setValue(code);
-                    }
-                    notification.success("Generated successfully");
+        ],function (id,e) {
+            switch (id) {
+                case 0:
+                    code+=lc;
+                    break;
+                case 1:
+                    code=lc+"\n"+code;
+                    break;
+                case 2:
+                    code=lc;
+                    break;
+                case 3:
+                default:
                     dialog.close();
-                });
+                    return;
             }
-            if(!goneToDialog){
-                projectCode.loop=code;
-                if($("#codeParts span[data-selected=true]").attr("data-name")=="loop"){
-                    monacoInstance.setValue(code);
-                }
-                notification.success("Generated successfully");
+            projectCode.loop=code;
+            if($("#codeParts span[data-selected=true]").attr("data-name")=="loop"){
+                monacoInstance.setValue(code);
             }
+            notification.success("Generated successfully");
+            dialog.close();
+        });
+    }
+    if(!goneToDialog){
+        projectCode.loop=code;
+        if($("#codeParts span[data-selected=true]").attr("data-name")=="loop"){
+            monacoInstance.setValue(code);
         }
+        notification.success("Generated successfully");
     }
 }

res/ui/toolbar/handlers/elements/duplicate.js
@@ -1,20 +1,20 @@
 'use strict';
-function LP_GV_Toolbar_Handler_5(){
-    toolBar.handlers.elements = {
-        duplicate (e) {
-            if(elements.selectedElement!=-1) {
-                var dt=elements.data[elements.selectedElement];
-                if(dt.type=="Slider"){
-                    dt.title+=" - Copy";
-                }
-                else{
-                    dt.text+=" - Copy";
-                }
-                elements.create(dt);
-            }
-            else{
-                notification.error("Cannot duplicate, no element selected")
-            }
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.elements= toolBar.handlers.elements || {};
+
+toolBar.handlers.elements.duplicate = function(e) {
+    if(elements.selectedElement!=-1) {
+        var dt=elements.data[elements.selectedElement];
+        if(dt.type=="Slider"){
+            dt.title+=" - Copy";
         }
+        else{
+            dt.text+=" - Copy";
+        }
+        elements.create(dt);
+    }
+    else{
+        notification.error("Cannot duplicate, no element selected")
     }
 }

res/ui/toolbar/handlers/file/generateCode.js
@@ -1,37 +1,40 @@
-function LP_GV_Toolbar_Handler_7() {
-    toolBar.handlers.file.generateCode= function (e) {
-        var p= localStorage.getItem("generateCodePath") || "gui900.h";
-        var path=prompt("Please enter the path of Gui900 library, to include it in the generated code.\nThis file name will be used in an #include statement, you can change it later.",p);
-        if(!path)return;
-        localStorage.setItem("generateCodePath", path);
-        var code=generateCode(path);
+'use strict';
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.file= toolBar.handlers.file || {};
 
-        var el=$("<div></div>").css("height", "300px");
-        monaco.editor.create(el[0],{...{
-            language: "cpp",
-            value: code,
-            automaticLayout: true,
-            theme: "GVE-"+$("html").attr('theme'),
-            readOnly: true,
-            minimap: {
-                enabled: false
-            },
-            scrollbar:{
-                alwaysConsumeMouseWheel: false
-            }
-        }, ...(JSON.parse(localStorage.getItem("monacoGlobalOptions"))),
-           ...(JSON.parse(localStorage.getItem("monacoCppOptions"))),
-           ...(JSON.parse(localStorage.getItem("monacoGenerateCodeResultOptions")))});
-        
-        
-        dialog(el, [
-            {
-                "id": 0,
-                "text": "OK"
-            }
-        ], function () {
-            dialog.close();
-        });
-        $("#dialog .content").addClass(["doNotTouchCss","defaultCursor"]);
-    }
+toolBar.handlers.file.generateCode= function (e) {
+    var p= localStorage.getItem("generateCodePath") || "gui900.h";
+    var path=prompt("Please enter the path of Gui900 library, to include it in the generated code.\nThis file name will be used in an #include statement, you can change it later.",p);
+    if(!path)return;
+    localStorage.setItem("generateCodePath", path);
+    var code=generateCode(path);
+
+    var el=$("<div></div>").css("height", "300px");
+    monaco.editor.create(el[0],{...{
+        language: "cpp",
+        value: code,
+        automaticLayout: true,
+        theme: "GVE-"+$("html").attr('theme'),
+        readOnly: true,
+        minimap: {
+            enabled: false
+        },
+        scrollbar:{
+            alwaysConsumeMouseWheel: false
+        }
+    }, ...(JSON.parse(localStorage.getItem("monacoGlobalOptions"))),
+        ...(JSON.parse(localStorage.getItem("monacoCppOptions"))),
+        ...(JSON.parse(localStorage.getItem("monacoGenerateCodeResultOptions")))});
+    
+    
+    dialog(el, [
+        {
+            "id": 0,
+            "text": "OK"
+        }
+    ], function () {
+        dialog.close();
+    });
+    $("#dialog .content").addClass(["doNotTouchCss","defaultCursor"]);
 }

res/ui/toolbar/handlers/file/import-from-json.js
@@ -1,26 +1,23 @@
 'use strict';
-function LP_GV_Toolbar_Handler_1(){
-    toolBar.handlers={
-        file: {
-            open: {
-                import_from_json (e) {
-                    dialog("Please paste the JSON code here: <textarea id='dialog-json-input' rows='8'></textarea>",[
-                        {
-                            "id": 0,
-                            "text": "Cancel"
-                        },
-                        {
-                            "id": 1,
-                            "text": "Continue"
-                        }
-                    ],function (id,e) {
-                        if(id==1){
-                            fileIO.load($("#dialog-json-input").val());
-                        }
-                        dialog.close();
-                    })
-                }
-            }
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.file= toolBar.handlers.file || {};
+toolBar.handlers.file.open= toolBar.handlers.file.open || {};
+
+toolBar.handlers.file.open.import_from_json = function(e) {
+    dialog("Please paste the JSON code here: <textarea id='dialog-json-input' rows='8'></textarea>",[
+        {
+            "id": 0,
+            "text": "Cancel"
+        },
+        {
+            "id": 1,
+            "text": "Continue"
         }
-    }
+    ],function (id,e) {
+        if(id==1){
+            fileIO.load($("#dialog-json-input").val());
+        }
+        dialog.close();
+    })
 }

res/ui/toolbar/handlers/file/save.js
@@ -1,34 +1,37 @@
-function LP_GV_Toolbar_Handler_6(){
-    toolBar.handlers.file.save = {
-        saveToFile (e) {
-            // https://stackoverflow.com/a/30832210/13561926 deleted IE code
-            // Function to download data to a file
-            function download(data, filename, type) {
-                var file = new Blob([data], {type: type});
-                
-                var a = document.createElement("a"),
-                        url = URL.createObjectURL(file);
-                a.href = url;
-                a.download = filename;
-                document.body.appendChild(a);
-                a.click();
-                setTimeout(function() {
-                    document.body.removeChild(a);
-                    window.URL.revokeObjectURL(url);  
-                }, 0); 
-            }
+'use strict';
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.file= toolBar.handlers.file || {};
+toolBar.handlers.file.save= toolBar.handlers.file.save || {};
 
-            download(JSON.stringify(fileIO.createJSON()), "Untitled.gui900", "gui900")
-        },
-        saveToJSON (e) {
-            dialog(("<textarea style='height: 300px' readonly>"+JSON.stringify(fileIO.createJSON())+"</textarea>"), [
-                {
-                    "id": 0,
-                    "text": "OK"
-                }
-            ], function () {
-                dialog.close();
-            })
-        }
+toolBar.handlers.file.save.saveToFile = function(e) {
+    // https://stackoverflow.com/a/30832210/13561926 deleted IE code
+    // Function to download data to a file
+    function download(data, filename, type) {
+        var file = new Blob([data], {type: type});
+        
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
     }
+
+    download(JSON.stringify(fileIO.createJSON()), "Untitled.gui900", "gui900")
+}
+
+toolBar.handlers.file.save.saveToJSON = function(e) {
+    dialog(("<textarea style='height: 300px' readonly>"+JSON.stringify(fileIO.createJSON())+"</textarea>"), [
+        {
+            "id": 0,
+            "text": "OK"
+        }
+    ], function () {
+        dialog.close();
+    })
 }

res/ui/toolbar/handlers/help/about.js
@@ -1,20 +1,20 @@
 'use strict';
-function LP_GV_Toolbar_Handler_3(){
-    toolBar.handlers.help= {
-        about (e) {
-            dialog(
-                `Gui900 UI Editor 0.1.0<br>
-                By <a href="https://github.com/Mammad900" target="_blank">Mammad900</a><br>
-                JQuery 3.5.1<br>
-                JQuery UI 1.10.3<br>
-                Monaco editor 0.21.3<br>
-                Font Awesome Free 5.15.2`,[
-                {
-                    "id": 0,
-                    "text": "OK",
-                    "key": 27
-                }
-            ],function (id,e) {dialog.close();}, true)
+var toolBar= toolBar || {};
+toolBar.handlers= toolBar.handlers || {};
+toolBar.handlers.help= toolBar.handlers.help || {};
+
+toolBar.handlers.help.about = function(e) {
+    dialog(
+        `Gui900 UI Editor 0.1.0<br>
+        By <a href="https://github.com/Mammad900" target="_blank">Mammad900</a><br>
+        JQuery 3.5.1<br>
+        JQuery UI 1.10.3<br>
+        Monaco editor 0.21.3<br>
+        Font Awesome Free 5.15.2`,[
+        {
+            "id": 0,
+            "text": "OK",
+            "key": 27
         }
-    }
+    ],function (id,e) {dialog.close();}, true)
 }

commit 218aece7b182b6f7bb603bc61a815e8e140b13f0

Fix bugs introduced with 8cb12bf

res/loader.js
@@ -126,15 +126,6 @@ async function LP(){
     try{
         var LP_funcs=[
             LP_SetTheme,
-
-            LP_GV_Toolbar_Handler_1,
-            LP_GV_Toolbar_Handler_2,
-            LP_GV_Toolbar_Handler_3,
-            LP_GV_Toolbar_Handler_4,
-            LP_GV_Toolbar_Handler_5,
-            LP_GV_Toolbar_Handler_6,
-            LP_GV_Toolbar_Handler_7,
-            LP_GV_Toolbar_Handler_8,
             
             LP_keyBindings,
             LP_HandleNewElementButton,

res/ui/toolbar/handlers/file/open-from-file.js
@@ -4,7 +4,7 @@ toolBar.handlers= toolBar.handlers || {};
 toolBar.handlers.file= toolBar.handlers.file || {};
 toolBar.handlers.file.open= toolBar.handlers.file.open || {};
 
-toolBar.handlers.file.open_from_file=function (e) {
+toolBar.handlers.file.open.open_from_file=function (e) {
     $("#fileinput").trigger("click");
 }
 

res/ui/toolbar/items.js
@@ -3,7 +3,7 @@ function LP_GenerateToolbar() {
     var toolbar = document.getElementById("toolbar");
     toolBar.menuItem(toolbar, "File", function (e) {}, function (file) {
         toolBar.subMenuItem(file, "Open", "fa-folder-open", function (e) {}, function (open) {
-            toolBar.subMenuItem(open, "Open from file", "fa-folder-open", toolBar.handlers.open_from_file);
+            toolBar.subMenuItem(open, "Open from file", "fa-folder-open", toolBar.handlers.file.open.open_from_file);
             toolBar.subMenuItem(open, "Open from JSON code", "fa-code", toolBar.handlers.file.open.import_from_json);
             toolBar.subMenuItem(open, "Open from browser storage", "fa-database", function () {
                 localStorage.getItem("savedProject") ? fileIO.load(localStorage.getItem("savedProject")) : notification.error("There is no saved project in browser storage.");