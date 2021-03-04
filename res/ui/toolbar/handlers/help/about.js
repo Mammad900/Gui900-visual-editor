2ea123a875228dc556b99bf131c5dc6a185bb000
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-03-04 22:52:21 +0330

    Update about.js

--- res/ui/toolbar/handlers/help/about.js
+++ res/ui/toolbar/handlers/help/about.js
@@ -5,7 +5,7 @@ toolBar.handlers.help= toolBar.handlers.help || {};
 
 toolBar.handlers.help.about = function(e) {
     dialog(
-        `Gui900 UI Editor 0.1.0<br>
+        `Gui900 Visual Editor 0.1.0<br>
         By <a href="https://github.com/Mammad900" target="_blank">Mammad900</a><br>
         JQuery 3.5.1<br>
         JQuery UI 1.10.3<br>
