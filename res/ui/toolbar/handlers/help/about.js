bd826537d0d7fee305a5d06975c4ee4da1fb6ddd
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-03-06 17:12:00 +0330

    Fix wrong jQuery-UI version in about dialog

--- res/ui/toolbar/handlers/help/about.js
+++ res/ui/toolbar/handlers/help/about.js
@@ -8,7 +8,7 @@ toolBar.handlers.help.about = function(e) {
         `Gui900 Visual Editor 0.1.0<br>
         By <a href="https://github.com/Mammad900" target="_blank">Mammad900</a><br>
         JQuery 3.5.1<br>
-        JQuery UI 1.10.3<br>
+        JQuery UI 1.12.1<br>
         Monaco editor 0.21.3<br>
         Font Awesome Free 5.15.2`,[
         {
