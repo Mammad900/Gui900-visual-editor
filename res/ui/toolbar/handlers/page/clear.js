c5bbfb34d6396c5626e8a0f449fc606ece69fd87
Mammad900 <mohammadhosseinabedi052@gmail.com>
2021-03-24 19:34:15 +0430

    Confirmation dialog on clearing page

--- res/ui/toolbar/handlers/page/clear.js
+++ res/ui/toolbar/handlers/page/clear.js
@@ -4,7 +4,24 @@ toolBar.handlers= toolBar.handlers || {};
 toolBar.handlers.pages= toolBar.handlers.pages || {};
 
 toolBar.handlers.pages.clear = function(e) {
-    while (elements.data.length!=0) {
-        elements.table.buttons.actions.delete(0,false)
-    }
+    
+    dialog("Do you really want to <strong>delete</strong> this element?<br>This cannot be undone.",[
+        {
+            "id": 0,
+            "text": "No",
+            "key": 27
+        },
+        {
+            "id": 1,
+            "text": "Yes",
+            "key": 13
+        }
+    ],function (id,e) {
+        if(id==1){
+            while (elements.data.length!=0) {
+                elements.table.buttons.actions.delete(0,false)
+            }
+        }
+        dialog.close();
+    })
 }
